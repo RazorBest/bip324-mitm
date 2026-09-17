ecf33d2f43b741d48a630f6856dbf1c2a08a15b6
Razor <22615594+RazorBest@users.noreply.github.com>
2026-09-17 22:26:53 +0200

    test: test_packet_relay_steppped_handshake_garbage_includes_terminator

--- src/lib.rs
+++ src/lib.rs
@@ -3249,6 +3249,120 @@ mod mitmbip324_component_tests {
     }
 
     #[test]
+    fn test_packet_relay_steppped_handshake_garbage_includes_terminator() {
+        let mut rng = secp256k1::rand::thread_rng();
+        let (mut comps, client_key, client_garbage, _server_key, server_garbage) =
+            new_components(&mut rng);
+        comps.mitm.enable_packet_relay();
+        comps.mitm.ensure_terminator_not_split(false).unwrap();
+        // This will make the mitm relay garbage and terminator bytes immediately
+        comps.mitm.ensure_garbage_includes_terminator(true).unwrap();
+        comps
+            .client_reader
+            .ensure_terminator_not_split(false)
+            .unwrap();
+        comps
+            .server_reader
+            .ensure_terminator_not_split(false)
+            .unwrap();
+
+        // Client -- key --> Server
+        client_to_server(&mut comps, NUM_ELLIGATOR_SWIFT_BYTES);
+        let packet = comps.mitm.next_client_protocol_packet().unwrap().unwrap();
+        assert!(matches!(packet, PHS!(HKey(..))));
+        assert_eq!(packet, HandshakeKey!(client_key.elligator_swift.to_array()));
+        let maybe_packet = comps.mitm.next_client_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        comps.client_writer.push_garbage_bytes(&client_garbage);
+        comps.client_writer.set_garbage_eof();
+
+        // Client -- garbage without 1 byte --> MITM
+        client_to_mitm(&mut comps, client_garbage.len() - 1);
+        let maybe_packet = comps.mitm.next_client_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // MITM -- partial garbage --> Server
+        let drained = drain_mitm_to_server(&mut comps);
+        // All partial garbage should be relayed
+        assert_eq!(drained, client_garbage.len() - 1);
+
+        // Client -- last byte of garbage --> MITM
+        client_to_mitm(&mut comps, 1);
+
+        // MITM -- last gabage byte --> Server
+        let drained = drain_mitm_to_server(&mut comps);
+        assert_eq!(drained, 1);
+        let maybe_packet = comps.mitm.next_client_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // Server -- key --> Client
+        server_to_client(&mut comps, NUM_ELLIGATOR_SWIFT_BYTES);
+        let packet = comps.mitm.next_server_protocol_packet().unwrap().unwrap();
+        assert!(matches!(packet, PHS!(HKey(..))));
+
+        // Client -- terminator without 1 byte --> MITM
+        client_to_mitm(&mut comps, NUM_GARBAGE_TERMINATOR_BYTES - 1);
+        let maybe_packet = comps.mitm.next_client_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // MITM -- terminator wihtout 1 byte --> Server
+        let drained = drain_mitm_to_server(&mut comps);
+        assert_eq!(drained, NUM_GARBAGE_TERMINATOR_BYTES - 1);
+        // The mitm module can't confirm the garbage has ended unless it received the entire
+        // terminator
+        let maybe_packet = comps.mitm.next_client_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // Client -- last byte of terminator --> MITM
+        client_to_mitm(&mut comps, 1);
+        let packet = comps.mitm.next_client_protocol_packet().unwrap().unwrap();
+        assert_eq!(packet, HandshakeGarb!(client_garbage));
+        let packet = comps.mitm.next_client_protocol_packet().unwrap().unwrap();
+        assert!(matches!(packet, PHS!(HTerm(..))));
+        let maybe_packet = comps.mitm.next_client_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // MITM -- terminator --> Server
+        let drained = drain_mitm_to_server(&mut comps);
+        assert_eq!(drained, 1);
+
+        comps.server_writer.push_garbage_bytes(&server_garbage);
+        comps.server_writer.set_garbage_eof();
+
+        // Server -- garbage + terminator without 1 byte --> MITM
+        server_to_mitm(
+            &mut comps,
+            server_garbage.len() + NUM_GARBAGE_TERMINATOR_BYTES - 1,
+        );
+        let maybe_packet = comps.mitm.next_server_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        let drained = drain_mitm_to_client(&mut comps);
+        assert_eq!(
+            drained,
+            server_garbage.len() + NUM_GARBAGE_TERMINATOR_BYTES - 1
+        );
+        let maybe_packet = comps.mitm.next_server_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // Server -- last byte of terminator --> MITM
+        server_to_mitm(&mut comps, 1);
+        let packet = comps.mitm.next_server_protocol_packet().unwrap().unwrap();
+        assert_eq!(packet, HandshakeGarb!(server_garbage));
+        let packet = comps.mitm.next_server_protocol_packet().unwrap().unwrap();
+        assert!(matches!(packet, PHS!(HTerm(..))));
+        let maybe_packet = comps.mitm.next_server_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+
+        // MITM -- last byte of terminator --> Client
+        let drained = drain_mitm_to_client(&mut comps);
+        assert_eq!(drained, 1);
+        let maybe_packet = comps.mitm.next_server_protocol_packet().unwrap();
+        assert!(maybe_packet.is_none());
+    }
+
+    #[test]
     fn test_packet_relay_data() {
         let mut rng = secp256k1::rand::thread_rng();
         let (mut comps, _client_key, client_garbage, _server_key, server_garbage) =
